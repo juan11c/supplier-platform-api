1ca699fb1903b34670d81d6e30e6e8d43b745b44
Juan <juan.cabello1@outlook.com>
2026-08-10 17:35:53 -0600

    Agregar archivos de proyecto.

---
+++ SupplierPlatform.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SupplierPlatform.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SupplierPlatform.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SupplierPlatform.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
