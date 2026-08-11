68ad3d049a2695859520a213455fafe545540367
Juan <juan.cabello1@outlook.com>
2026-08-11 16:46:46 -0600

    feat(infrastructure): implementar DbContext, Fluent API y repositorio de proveedores

--- SupplierPlatform.Infrastructure/Persistence/ApplicationDbContext.cs
+++ SupplierPlatform.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using SupplierPlatform.Domain.Entities;
 
-namespace SupplierPlatform.Infrastructure.Persistence
+namespace SupplierPlatform.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
 {
-    internal class ApplicationDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
+    }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<SupplierProfile> SupplierProfiles => Set<SupplierProfile>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Aplica automáticamente las configuraciones de la carpeta Configurations
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
--- SupplierPlatform.Infrastructure/Persistence/Repositories/SupplierRepository.cs
+++ SupplierPlatform.Infrastructure/Persistence/Repositories/SupplierRepository.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using SupplierPlatform.Application.Interfaces.Repositories;
+using SupplierPlatform.Domain.Entities;
 
-namespace SupplierPlatform.Infrastructure.Persistence.Repositories
+namespace SupplierPlatform.Infrastructure.Persistence.Repositories;
+
+public class SupplierRepository : ISupplierRepository
 {
-    internal class SupplierRepository
+    private readonly ApplicationDbContext _context;
+
+    public SupplierRepository(ApplicationDbContext context)
     {
+        _context = context;
+    }
+
+    public async Task<SupplierProfile?> GetByIdAsync(Guid id)
+    {
+        return await _context.SupplierProfiles
+            .Include(s => s.User)
+            .FirstOrDefaultAsync(s => s.Id == id);
+    }
+
+    public async Task<SupplierProfile?> GetByClaimTokenAsync(string token)
+    {
+        return await _context.SupplierProfiles
+            .FirstOrDefaultAsync(s => s.ClaimToken == token);
+    }
+
+    public async Task AddAsync(SupplierProfile supplier)
+    {
+        await _context.SupplierProfiles.AddAsync(supplier);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(SupplierProfile supplier)
+    {
+        _context.SupplierProfiles.Update(supplier);
+        await _context.SaveChangesAsync();
     }
 }
